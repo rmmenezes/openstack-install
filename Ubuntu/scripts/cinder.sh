1593a9917ded62eb80c78492988c5780b35bc8ba
rmmenezes <ra29fa@gmail.com>
2021-09-22 16:47:41 -0300

    .

--- Ubuntu/scripts/cinder.sh
+++ Ubuntu/scripts/cinder.sh
@@ -12,25 +12,29 @@ openstack role add --project service --user cinder admin
 openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
 openstack service create --name cinderv3 --description "OpenStack Block Storage" volumev3
 
-openstack endpoint create --region RegionOne volumev2 public http://cinder:8776/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volumev2 internal http://cinder:8776/v2/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volumev2 admin http://cinder:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 public http://cinder:8776/v2/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev2 internal http://cinder:8776/v2/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev2 admin http://cinder:8776/v2/%\(project_id\)s
 
-openstack endpoint create --region RegionOne volumev3 public http://cinder:8776/v3/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volumev3 internal http://cinder:8776/v3/%\(tenant_id\)s
-openstack endpoint create --region RegionOne volumev3 admin http://cinder:8776/v3/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev3 public http://cinder:8776/v3/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev3 internal http://cinder:8776/v3/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev3 admin http://cinder:8776/v3/%\(project_id\)s
 
-sudo DEBIAN_FRONTEND=noninteractive apt install cinder-api cinder-scheduler -y
+apt install cinder-api cinder-scheduler -y
 
 mv /etc/cinder/cinder.conf /etc/cinder/cinder.conf.original
 cp ./files/cinder/cinder.conf /etc/cinder/cinder.conf
 chgrp cinder /etc/cinder/cinder.conf
 
 su -s /bin/sh -c "cinder-manage db sync" cinder
+
+sudo systemctl restart nova-api
 systemctl restart cinder-scheduler
+sudo systemctl restart apache2
+
 openstack volume service list
 
-sudo DEBIAN_FRONTEND=noninteractive apt install lvm2 thin-provisioning-tools -y
+apt install lvm2 tgt thin-provisioning-tools -y
 
 #monta o disco
 pvcreate /dev/vdb
@@ -38,7 +42,11 @@ vgcreate cinder-volumes /dev/vdb
 
 sed -i '/devices {$/a filter = [ "a/vdb/", "r/.*/"]' /etc/lvm/lvm.conf
 
-sudo DEBIAN_FRONTEND=noninteractive apt install cinder-volume -y
-systemctl restart cinder-volume
+apt install cinder-volume -y
+
+sudo systemctl restart tgt
+sudo systemctl restart cinder-volume
+
+openstack volume service list
 
 echo "FIM !"
